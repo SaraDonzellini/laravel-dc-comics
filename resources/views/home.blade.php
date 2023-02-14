9bccccf437b87533e3a4a1bc14fb0ae30a5ace71
SaraDonzellini <saradonzie@gmail.com>
2023-02-14 14:21:02 +0100

    added lorem tohome page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,11 @@
       <h1>
         Homepage DC Comics
       </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur deserunt officiis molestiae consectetur fuga ea excepturi ex velit quam provident cumque voluptatibus eveniet sit, expedita reprehenderit minima nulla itaque quae!
+        Cumque in nulla omnis corporis incidunt qui vero consequuntur necessitatibus ex, odio placeat numquam nam vel quis praesentium sapiente aperiam fuga quae nesciunt impedit asperiores! Officiis, dicta vitae. Earum, corrupti.
+        Quis accusantium, ipsam temporibus incidunt voluptate nulla at ducimus nihil quam optio. Nemo ullam ratione necessitatibus accusamus velit assumenda veritatis dolores est quae. Cum repellendus aut vero nihil iure architecto.
+      </p>
     </div>
   </div>
 </section>
